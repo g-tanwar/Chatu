ca2f6cc6065b8bf67b9001a169778a270e22a7f2
g-tanwar <tanwarg282005@gmail.com>
2025-09-15 01:40:04 +0530

    updated db credentials

--- server/src/database/database.providers.ts
+++ server/src/database/database.providers.ts
@@ -9,11 +9,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'postgres',
-        host: 'localhost',
+        host: 'dpg-d33hh4adbo4c73b80fu0-a.oregon-postgres.render.com',
         port: 5432,
-        username: 'postgres',
+        username: 'chatu_user',
         password: process.env.DB_PASSWORD,
-        database: 'database_development'
+        database: 'chatu'
       });
       sequelize.addModels([User, Message, Channel]);
       await sequelize.sync();
