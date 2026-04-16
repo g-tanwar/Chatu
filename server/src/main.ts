465a103f7872e2aaed2b7b836d1b34362225cfda
“Gourav <tanwarg282005@gmail.com>
2026-04-16 23:50:53 +0530

    Use PORT environment variable for Render deployment

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.enableCors();
-  await app.listen(5001);
+  await app.listen(process.env.PORT || 5001);
 }
 bootstrap();
